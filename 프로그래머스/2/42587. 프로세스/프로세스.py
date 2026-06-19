f67bc86355a0af1ae3f2f35cfe8f4bda7e31575d
nomad1jin <167287284+nomad1jin@users.noreply.github.com>
2026-06-19 19:38:01 +0900

    [level 2] Title: 프로세스, Time: 0.34 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = 0
+    answer = 0      # 인쇄 순서
     q = deque(priorities)
     
     while q:
@@ -9,16 +9,19 @@ def solution(priorities, location):
         pop_n = q.popleft()
         location -= 1  # 하나 꺼냈으니 위치 - 1
         
+        # 뒤로 보내기
         if pop_n < max_n:
-            q.append(pop_n) # 튜플
+            q.append(pop_n)
             
             # 만약 꺼내기 전 문서가 인덱스 0이라면 -1이 됨. 
             # 추가되었으니 이제 마지막 인덱스가 됨
             if location < 0:
                 location = len(q) - 1
-            
+        
+        # 출력 및 순서 
         else :
-            # 없으면 프로세스 실행
+            # 없으면 프로세스 실행, 그런데 location이 음수가 되면. 
+            # 해당 location의 프로세스가 답이므로 리턴
             answer += 1
             if location < 0:
                 return answer
